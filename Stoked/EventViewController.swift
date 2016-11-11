c230af4356e9d15a9d46d725441d7ff815d0e109
Thomas Gross Rasmussen <thomasgrossrasmussen@gmail.com>
2016-11-11 19:18:23 +0100

    Update EventViewController.swift

--- Stoked/EventViewController.swift
+++ Stoked/EventViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class EventViewController: UIViewController {
 
-    @IBOutlet weak var SideMenuButton: UIBarButtonItem!
+    @IBOutlet weak var SideMenuButton: UIBarButtonItem!aasd
     
     override func viewDidLoad() {
         super.viewDidLoad()
