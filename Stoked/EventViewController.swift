0d22b610faf5e1cac5848757ef561eb051024caa
ThomasGross <thomasgrossrasmussen@gmail.com>
2016-11-11 13:22:47 +0100

    Initial Commit

---
+++ Stoked/EventViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Stoked
+//
+//  Created by Thomas Rasmussen on 11/11/2016.
+//  Copyright © 2016 Thomas Rasmussen. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+
+    @IBOutlet weak var SideMenuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        SideMenuButton.target = self.revealViewController()
+        SideMenuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Stoked/MapViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  Stoked
+//
+//  Created by Thomas Rasmussen on 10/11/2016.
+//  Copyright © 2016 Thomas Rasmussen. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var SideMenuButton: UIBarButtonItem!
+    
+    @IBOutlet weak var MapView: MKMapView!
+    
+    // tells the whether or not to update userlocation
+    var relocation : Bool = false
+    
+    // Delegate property for the locationmanager
+    var locationManager = CLLocationManager()
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        SideMenuButton.target = self.revealViewController()
+        SideMenuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+        // sets the delegate to be self
+        locationManager.delegate = self
+        
+        // set the disired location to be best possible
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // We only want to use locationmanager when the app is used
+        locationManager.requestWhenInUseAuthorization()
+        
+    }
+    
+    // called when the region is changed - only in the start of the app because of relocation if statement
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        if relocation {
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            
+            // method that start looking for a location
+            locationManager.startUpdatingLocation()
+            print("started updating map")
+            MapView.showsUserLocation = true
+            relocation = true
+            
+            
+        default:
+            locationManager.stopUpdatingLocation()
+            print("stopped updating map")
+            MapView.showsUserLocation = false
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let userLocation = locations.last{  // optional binding
+            
+            let region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate,100000,100000)
+
+            self.MapView.setRegion(region, animated: true) //vis området på kortet
+        }
+        
+        if relocation{
+            locationManager.stopUpdatingLocation()
+            relocation = false
+        }
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
